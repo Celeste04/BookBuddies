568c1df57ae7f19f2bd38e5b8c63c8bed6383ebb
Jessica Chen <jesschen046@gmail.com>
2024-01-13 20:58:05 -0500

    flask app

---
+++ ai.py
@@ -0,0 +1,41 @@
+import requests
+import app
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API_BASE_URL = "https://api.cloudflare.com/client/v4/accounts/d40839592c82f678e6a1eccbefc3e985/ai/run/"
+headers = {"Authorization": "Bearer "+ os.getenv('API_KEY')}
+
+def run(model, inputs):
+    input = { "messages": inputs }
+    response = requests.post(f"{API_BASE_URL}{model}", headers=headers, json=input)
+    full_result = response.json()
+    response_only = full_result.get('result', {}).get('response', '')
+    return response_only
+
+# get string of skills from resume
+condensed = "my skills are "
+
+def getfile(file) :
+    skill_list = app.extract_skills_from_resume()
+    for item in skill_list :
+        condensed += item
+        condensed += " "
+
+# interview questions
+inputs = [
+    { "role": "system", "content": "You are an assistant that helps provide interview questions" },
+    { "role": "user", "content": condensed}
+]
+
+output = run("@cf/meta/llama-2-7b-chat-int8", inputs)
+print(output) 
+
+# answers provided by user
+answers = [ 
+    { "role": "system", "content": "You are an assistant that provides constructive critism on the interview answers given and if they're good answers" },
+    { "role": "user", "content": "I worked on a python verison of the pokemon game. IT was really hard and I don't derserve this position."}
+]
+
+output = run("@cf/meta/llama-2-7b-chat-int8", answers)
+print(output) 
---
+++ app.py
@@ -0,0 +1,87 @@
+from pdfminer.high_level import extract_text
+import docx2txt
+import nltk
+import pandas as pd
+import os
+
+# RETRIEVING SKILLS
+
+CSV_PATH = 'skills.csv'
+df = pd.read_csv(CSV_PATH)
+df['skill_name'] = df['skill_name'].str.replace('-', ' ')
+skill_set = set(df['skill_name'])
+
+nltk.download('stopwords')
+nltk.download('punkt')
+file_path=''
+def get_file_path():
+    return file_path
+# Checking for pdf or docs
+def get_file_extension(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension.lower()
+
+def extract_text_pdf(pdf_path):
+    return extract_text(pdf_path)
+
+def extract_text_from_docx(docx_path):
+    txt = docx2txt.process(docx_path)
+    if txt:
+        return txt.replace('\t', ' ')
+    return None
+
+
+def extract_skills(input_text):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    word_tokens = nltk.tokenize.word_tokenize(input_text)
+    #remove the stop words
+    filtered_tokens = [w for w in word_tokens if w not in stop_words]
+
+    filtered_tokens = [w for w in word_tokens if w.isalpha()]
+
+    bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens,2,3)))
+
+    found_skills = set()
+
+    for token in filtered_tokens:
+        if token.lower() in skill_set:
+            found_skills.add(token)
+    
+    for ngram in bigrams_trigrams:
+        if ngram.lower() in skill_set:
+            found_skills.add(ngram)
+    
+    return found_skills
+
+def extract_skills_from_resume(text_path):
+    #text_path = input('Enter Resume Path: ') 
+    text = ''
+    if get_file_extension(text_path) == '.docx':
+        text = extract_text_from_docx(text_path)
+
+    elif get_file_extension(text_path) == '.pdf':
+        text = extract_text_pdf(text_path)
+    skills = extract_skills(text)
+    return skills
+
+def matchingSkills(res_path, job_desc): # job_desc is a string
+    matched = {} # set of matched jobs
+    res_skills = extract_skills_from_resume(res_path)
+    job_skills = extract_skills(job_desc)
+    for skill in job_skills:
+        if skill in res_skills:
+            matched.add(skill)
+    return matched
+
+def missingSkills(res_path, job_desc):
+    missing = {}
+    res_skills = extract_skills_from_resume(res_path)
+    job_skills = extract_skills(job_desc)
+    for skill in job_skills:
+        if skill not in res_skills:
+            missing.add(skill)
+    return missing
+
+def skillmatch(skillset1, skillset2):
+    return ( skillset1.size() // skillset2.size() ) * 100
+
