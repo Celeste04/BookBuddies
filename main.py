843d31d82a123f9501c8dd4b01cdc33d2361b12b
Jessica Chen <jesschen046@gmail.com>
2024-01-14 00:51:31 -0500

    uploading bugs fixed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
@@ -22,20 +22,19 @@ class UploadFileForm(FlaskForm):
 @app.route('/', methods=['GET',"POST"])
 @app.route('/home', methods=['GET',"POST"])
 def home():
-    if request.method == 'POST':
-        job_desc = request.form['user_input']
     form = UploadFileForm()
     current_skills = ''
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
+        uploaded_file = form.file.data # First grab the file
         #file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        file_path = os.path.join(UPLOAD_DIR, secure_filename(file.filename))
+        file_path = os.path.join(UPLOAD_DIR, secure_filename(uploaded_file.filename))
 
         # Ensure the 'uploads' directory exists, create it if necessary
         if not os.path.exists(UPLOAD_DIR):
             os.makedirs(UPLOAD_DIR)
         # Save file to uploads directory
-        file.save(file_path)
+        uploaded_file.save(file_path)
+        session['file_path'] = file_path
         current_skills = skills.extract_skills_from_resume(file_path)
     return render_template('index.html', form=form, current_skills=current_skills)
 
@@ -43,11 +42,11 @@ def home():
 def process_form():
     form=UploadFileForm()
     job_skills=''
-    print("yo")
+    file_path = session.get('file_path')
+    print("hi:", file_path)
     if request.method == 'POST':
         job_desc = request.form['jobRequirements']
         job_skills = skills.extract_skills(job_desc)
-    print("hi:",job_skills)
     return render_template('index.html', form=form, job_skills=job_skills)
 
 @app.route("/get", methods=["GET", "POST"])
@@ -61,5 +60,4 @@ def get_Chat_response(text):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
